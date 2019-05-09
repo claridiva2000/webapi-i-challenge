b5604ab1d809718f22af9c4bb476209d42e1918b
claridiva2000 <claridiva2000@gmail.com>
2019-05-08 22:55:49 -0500

    get and post done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+server.use(express.json());
+
+//send back list of all users
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(allUsers => {
+      res.json(allUsers);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({
+          err: `{ error: "The users information could not be retrieved." }`
+        });
+    });
+});
+
+server.post('/api/users', (req, res) => {
+  const newUser = req.body;
+  console.log('req body', req.body);
+  db.insert(newUser)
+    .then(addedUser => {
+      res.status(201).json(addedUser);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+
+
+
+
+server.listen(8500, () => {
+  console.log('I have no idea what i\'m doing');
+});
